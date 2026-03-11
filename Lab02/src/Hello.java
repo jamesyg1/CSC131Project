621b1fd8d043037ee0a75e8cf7f59c2c6af246d3
juana <juana@10.117.91.167>
2026-03-11 12:44:56 -0700

    added my info to line 9.

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
 		System.out.println("304130424 - This line was added by James Yang.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("303571437 - This line was added by Juan Macias Nunez.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
