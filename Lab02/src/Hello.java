16b678ed6b5c0ca2ad177ccb56493d0755ebdd05
izalesd <derick4546@gmail.com>
2026-03-30 13:08:43 -0700

    added Derick

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
 		System.out.println("304130424 - This line was added by James Yang.");
-		System.out.println("303571437 - This line was added by Juan Macias Nunez.");
+		System.out.println("304247801 - This line was added by Derick Izales-Urrea.");
 		System.out.println("303107883 - This line was added by Jahmeen Thomas.");
 		System.out.println("303871399 - This line was added by Mosnath Kalam .");
 		System.out.println("304050500 - This line was added by Alexander Gonzales Cupp.");
