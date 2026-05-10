3d4ef34c8e1a47c83e86a0a5a56f0f1bbdf3aaad
Grim Kyle <grimkylebis@gmail.com>
2026-05-10 11:58:35 -0700

    Did polishing and got rid of Main.java(not the Ui main.java)

--- CSC 131 Project/src/test/DatabaseTest.java
+++ CSC 131 Project/src/test/DatabaseTest.java
@@ -7,7 +7,7 @@ import java.sql.ResultSet;
 
 public class DatabaseTest {
     public static void main(String[] args) {
-        String url = "jdbc:sqlite:test.db";
+        String url = "jdbc:sqlite:receipt.db";
 
         try (Connection conn = DriverManager.getConnection(url)) {
             System.out.println("Test Connection Sucessful");
