dc6c5ebdc77b8766a1c31bc5bc2b00f886779d7b
Grim Kyle <grimkylebis@gmail.com>
2026-05-10 11:38:52 -0700

    Adding the search UI for receipt ID

--- CSC 131 Project/src/ui/LoginPage.java
+++ CSC 131 Project/src/ui/LoginPage.java
@@ -1,4 +1,5 @@
 package ui;
+
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
@@ -12,22 +13,21 @@ import javax.swing.JLabel;
 
 import model.User;
 
+public class LoginPage implements ActionListener {
 
-public class LoginPage implements ActionListener{
-	
 	JFrame frame = new JFrame();
 	JButton loginButton = new JButton("Login");
 	JButton registerButton = new JButton("Register");
+	JButton searchReceiptButton = new JButton("Search Receipt by ID");
 	JTextField userIDField = new JTextField();
 	JPasswordField userPassword = new JPasswordField();
 	JLabel userIDLabel = new JLabel("Email/Username:");
 	JLabel userPasswordLabel = new JLabel("Password:");
 	JLabel messageLabel = new JLabel();
-	HashMap<String,User> loginInfo = new HashMap<String,User>();
-	
-	LoginPage(HashMap<String,User> loginInfoOriginal){
-		
-		//Button layouts
+	HashMap<String, User> loginInfo = new HashMap<String, User>();
+
+	LoginPage(HashMap<String, User> loginInfoOriginal) {
+
 		loginInfo = loginInfoOriginal;
 		userIDLabel.setBounds(80, 160, 150, 25);
 		userPasswordLabel.setBounds(80, 220, 75, 25);
@@ -35,59 +35,64 @@ public class LoginPage implements ActionListener{
 		userPassword.setBounds(80, 245, 340, 35);
 		loginButton.setBounds(270, 310, 120, 35);
 		loginButton.addActionListener(this);
-		messageLabel.setBounds(80, 360, 340, 25);
-        messageLabel.setFont(new Font("Arial", Font.BOLD, 14));
+		messageLabel.setBounds(80, 410, 340, 25);
+		messageLabel.setFont(new Font("Arial", Font.BOLD, 14));
 		registerButton.setBounds(110, 310, 120, 35);
 		registerButton.addActionListener(this);
-		
-		//Buttons added onto frame
+		searchReceiptButton.setBounds(120, 360, 200, 30);
+		searchReceiptButton.addActionListener(this);
+
 		frame.add(registerButton);
 		frame.add(userIDLabel);
 		frame.add(userPasswordLabel);
 		frame.add(userIDField);
 		frame.add(userPassword);
 		frame.add(loginButton);
+		frame.add(searchReceiptButton);
 		frame.add(messageLabel);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(500,600);
+		frame.setSize(500, 600);
 		frame.setLayout(null);
 		frame.setVisible(true);
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent e){
+	public void actionPerformed(ActionEvent e) {
 
-	    if (e.getSource() == loginButton){
+		if (e.getSource() == loginButton) {
 
-	        String userID = userIDField.getText();
-	        String password = String.valueOf(userPassword.getPassword());
+			String userID = userIDField.getText();
+			String password = String.valueOf(userPassword.getPassword());
 
-	        User matchedUser = null;
+			User matchedUser = null;
 
-	        for (User user : loginInfo.values()){
-	            if ((user.getName().equals(userID) || user.getEmail().equals(userID)) && user.getPassword().equals(password)) {
-	                matchedUser = user;
-	                break;
-	            }
-	        }
+			for (User user : loginInfo.values()) {
+				if ((user.getName().equals(userID) || user.getEmail().equals(userID))
+						&& user.getPassword().equals(password)) {
+					matchedUser = user;
+					break;
+				}
+			}
 
-	        if (matchedUser != null){
-	            messageLabel.setForeground(Color.GREEN);
-	            messageLabel.setText("Login Successful");
-	            frame.dispose();
-	            new ReceiptPage(matchedUser);
-	        } else {
-	            messageLabel.setForeground(Color.RED);
-	            messageLabel.setText("Invalid email/username or password.");
-	        }
-	    }
+			if (matchedUser != null) {
+				messageLabel.setForeground(Color.GREEN);
+				messageLabel.setText("Login Successful");
+				frame.dispose();
+				new ReceiptPage(matchedUser);
+			} else {
+				messageLabel.setForeground(Color.RED);
+				messageLabel.setText("Invalid email/username or password.");
+			}
+		}
 
-	    if (e.getSource() == registerButton){
-	        frame.dispose();
-	        new RegisterPage(loginInfo);
-	    }
-		
+		if (e.getSource() == registerButton) {
+			frame.dispose();
+			new RegisterPage(loginInfo);
+		}
+
+		if (e.getSource() == searchReceiptButton) {
+			frame.dispose();
+			new SearchPage();
+		}
 	}
-	
-
 }
